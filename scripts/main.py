4f796546d14de5955f5152d0806c67d8e3fa40dc
WangMahua <whole9177j53@gmail.com>
2020-04-09 20:48:43 +0800

    0409 demo_goap

--- scripts/main.py
+++ scripts/main.py
@@ -18,15 +18,71 @@ def output_processor(output_action, left_side, right_side):
                 claw_num = 2 * (2 - left_side) + 8  # use layer 2 - right_side + '4'
                 output[claw_num-1] = 1
     if output_action.name is 'lift_left':
-        if left_side is 1:
-            output[12] = 3  # lift layer 3
-        else:
+        if left_side is 0:
+            output[12] = 0
+        elif left_side is 1:
+            output[12] = 1  # lift layer 3
+        elif left_side is 2:
             output[12] = 2  # lift layer 2
     elif output_action.name is 'lift_right':
+        if right_side is 0:
+            output[13] = 0
         if right_side is 1:
-            output[13] = 3  # lift layer 3
-        else:
+            output[13] = 1  # lift layer 3
+        elif right_side is 2:
             output[13] = 2  # lift layer 2
+    elif output_action.name is 'lower_right':
+        if right_side is 0:
+            output[12] = 0
+        if right_side is 1:
+            output[12] = 1  # lift layer 3
+        elif right_side is 2:
+            output[12] = 2  # lift layer 2
+    elif output_action.name is 'lower_right':
+        if right_side is 0:
+            output[13] = 0
+        if right_side is 1:
+            output[13] = 1  # lift layer 3
+        elif right_side is 2:
+            output[13] = 2  # lift layer 2
+    elif output_action.name is 'lower':
+        if right_side is 0:
+            output[12] = 0
+            output[13] = 0
+        if right_side is 1:
+            output[12] = 1
+            output[13] = 1  # lift layer 3
+        elif right_side is 2:
+            output[12] = 2
+            output[13] = 2  # lift layer 2
+    elif output_action.name is 'lift':
+        if right_side is 0:
+            output[12] = 0
+            output[13] = 0
+        if right_side is 1:
+            output[12] = 1
+            output[13] = 1  # lift layer 3
+        elif right_side is 2:
+            output[12] = 2
+            output[13] = 2  # lift layer 2
+    elif output_action.name is 'hand_down':
+        output[14] = 1
+    elif output_action.name is 'hand_up':
+        output[14] = 0
+    if output_action.name is 'open':
+        for p in output_action.preconditions:
+            if p is '1':
+                claw_num = 4*left_side   # use layer 2 - left_side + '1'
+                output[claw_num] = 0
+            elif p is '2':
+                claw_num = 4*left_side + 1  # use layer 2 - left_side + '2'
+                output[claw_num] = 0
+            elif p is'3':
+                claw_num = 4*right_side + 2  # use layer 2 - right_side + '3'
+                output[claw_num] = 0
+            else:
+                claw_num = 4*right_side + 3  # use layer 2 - right_side + '4'
+                output[claw_num] = 0
     return output
 
 
@@ -58,6 +114,16 @@ while len(demo_path) > 0:
             if path.name is 'lift_left':
                 left_side += 1
             elif path.name is 'lift_right':
+                right_side += 1
+            elif path.name is 'lower_left':
+                left_side -= 1
+            elif path.name is 'lower_right':
+                right_side -= 1
+            elif path.name is 'lower':
+                left_side -= 1
+                right_side -= 1
+            elif path.name is 'lift':
+                left_side += 1
                 right_side += 1
             path_done = True
             demo_path.remove(demo_path[0])
--- scripts/setting.py
+++ scripts/setting.py
@@ -1,27 +1,20 @@
-"""
-Set all actions, goals and current world states here!!
-this .py file is the only place you will need to change
-=======================================================
-"""
 from action import *
 
 
 def setting(mode):
     if mode is 1:  # name, preconditions, effects, position, cost, degree, speed, mode, speed_mode_radius, number, iscup
-        c1 = Action("cup1_1",      ['1', '3'], ['get_cup'], (200, 20), 3, 90, 150, 2, 0, 1, True)
-        c2 = Action("cup1_2",      ['2', '4'], ['get_cup'], (60, 50), 3, 90, 150, 2, 0, 1, True)
-        c3 = Action("cup2_1",      ['1'],      ['get_cup'], (12, 3), 3, 70, 150, 2, 0, 2, True)
-        c4 = Action('cup2_2',      ['1'],      ['get_cup'], (90, 90), 3, 40, 150, 2, 0, 2, True)
-        c5 = Action('cup3_1',      ['2'],      ['get_cup'], (15, 13), 3, 85, 150, 1, 0, 3, True)
-        c6 = Action('raise_flag',  ['flag'],   ['get_cup'], (3, 4), 3, 85, 150, 1, 0, 5, False)
-        c7 = Action('hand_down',   [],         ['flag'], (5, 5), 3, 85, 150, 1, 0, 5, False)
-        c8 = Action('cup4_1',      ['1'],      ['get_cup'], (3, 30), 3, 85, 150, 2, 15, 4, True)
-        c9 = Action('lift_left',   [],         ['lift_left', '1', '2'], (5, 5), 3, 85, 150, 2, 15, 6, False)
-        c10 = Action('lift_right', [],         ['lift_right', '3', '4'], (5, 5), 3, 85, 150, 2, 15, 6, False)
-        c11 = Action('put_cup',    [],         ['1', '2', '3', '4'], (90, 90), 3, 85, 150, 2, 15, 7, False)
+        c1 = Action("cup1",      ['2', '4'],   ['get_cup'], (455, 375), 3, 143, 150, 1, 0, 1, True)
+        c2 = Action("cup2",      ['1'],        ['get_cup'], (200, 670), 3, 90, 150, 1, 0, 2, True)
+        c3 = Action("cup3",      ['3'],        ['get_cup'], (300, 1010), 3, 60, 150, 1, 0, 3, True)
+        c4 = Action('lift',      [],           ['lift_left', '1', '2'], (300, 1010), 3, 60, 150, 1, 15, 6, False)
+        c5 = Action('hand_down', [],           [], (1800, 200), 3, 90, 150, 1, 15, 6, False)
+        c6 = Action('hand_up',   [],           [], (1800, 670), 3, 90, 150, 1, 15, 6, False)
+        c7 = Action('goto',      [],           [], (1650, 1265), 3, 270, 150, 1, 15, 6, False)
+        c8 = Action('lower',     [],           [], (1800, 270), 3, 60, 270, 1, 15, 6, False)
+        c9 = Action('open',      ['1', '2', '3', '4'], [], (1800, 270), 3, 270, 150, 1, 15, 6, True)
+        c10 = Action('goto',      [], [], (1650, 1265), 3, 270, 150, 1, 15, 6, False)
+        c11 = Action('goto', [], [], (600, 300), 3, 270, 150, 1, 15, 6, False)
 
         demo_path = [c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11]
 
     return demo_path
-
-

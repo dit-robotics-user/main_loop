8fc622290d7bac38f1145205d0435c0c3afa5137
WangMahua <whole9177j53@gmail.com>
2020-05-26 20:19:00 +0800

    0526_3 auto grab do not calculate next pos

--- src/main_v1.cpp
+++ src/main_v1.cpp
@@ -298,7 +298,7 @@ int main(int argc, char **argv)
     long int r1=0;
     long int r2=0;
     long int r3=0;
-    long int rx0=0;
+    long int rx0=4;
     long int rx1=0;
     long int rx2=0;
     long int rx3=0;

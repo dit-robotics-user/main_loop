9c21383af1af10969750b067c7b429f5ac543728
dit-robotics <60845896+dit-robotics@users.noreply.github.com>
2020-06-12 16:18:30 +0800

    Update demo.launch.cpp

--- demo.launch.cpp
+++ demo.launch.cpp
@@ -16,4 +16,17 @@
       <param  name="ally_y" type="int" value="6"/>
 
    </node>	
+
+   <node pkg="main_loop" type="goap_main_v1.py" name="goap_main_v1" respawn="true" >
+   </node>
+
+   <node pkg="main_loop" type="path_planning_" name="path_planning_" output="log" respawn="true">
+   </node>
+
+   <node pkg="serial_port" type="serial_port_node" name="serial_port_node" output="log" respawn="true">
+   </node>
+
+   <node pkg="serial_port" type="serial_port_node_2" name="serial_port_node_2" output="log" respawn="true">
+   </node>
+
 </launch>

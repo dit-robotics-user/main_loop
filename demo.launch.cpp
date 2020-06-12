f04a683892816946c671c2e7aa07d092d44bc111
dit-robotics <60845896+dit-robotics@users.noreply.github.com>
2020-06-12 23:25:00 +0800

    0612

--- demo.launch.cpp
+++ demo.launch.cpp
@@ -1,19 +1,19 @@
 <launch>
-   <node name = "main_frank" pkg = "main_loop" type="main_frank" output="screen">
+   <node name = "main_frank" pkg = "main_loop" type="main_frank" output="screen" respawn="true">
 
       <param  name="rx0" type="int" value="1489"/>
-      <param  name="rx1" type="int" value="56"/>
-      <param  name="rx2" type="int" value="56"/>
+      <param  name="rx1" type="int" value="5656"/>
+      <param  name="rx2" type="int" value="5"/>
       <param  name="rx3" type="int" value="6"/>
 
-      <param  name="goal_pos_x" type="int" value="1489"/>
-      <param  name="goal_pos_y" type="int" value="56"/>
-      <param  name="enemy1_x" type="int" value="56"/>
-      <param  name="enemy1_y" type="int" value="6"/>
-      <param  name="enemy2_x" type="int" value="6"/>
-      <param  name="enemy2_y" type="int" value="6"/>
-      <param  name="ally_x" type="int" value="6"/>
-      <param  name="ally_y" type="int" value="6"/>
+      <param  name="goal_pos_x" type="int" value="1000"/>
+      <param  name="goal_pos_y" type="int" value="1000"/>
+      <param  name="enemy1_x" type="int" value="1300"/>
+      <param  name="enemy1_y" type="int" value="1800"/>
+      <param  name="enemy2_x" type="int" value="5000"/>
+      <param  name="enemy2_y" type="int" value="5000"/>
+      <param  name="ally_x" type="int" value="1300"/>
+      <param  name="ally_y" type="int" value="1300"/>
 
    </node>	
 
@@ -23,10 +23,5 @@
    <node pkg="main_loop" type="path_planning_" name="path_planning_" output="log" respawn="true">
    </node>
 
-   <node pkg="serial_port" type="serial_port_node" name="serial_port_node" output="log" respawn="true">
-   </node>
-
-   <node pkg="serial_port" type="serial_port_node_2" name="serial_port_node_2" output="log" respawn="true">
-   </node>
 
 </launch>
